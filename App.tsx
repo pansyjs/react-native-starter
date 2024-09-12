a02968d77a3fe016b9e8d3e0c497fa52a2f562c2
wangxingkang <wang_xingkang@qq.com>
2024-09-12 23:38:30 +0800

    chore: used react-native 0.73.9

--- App.tsx
+++ App.tsx
@@ -1,3 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
