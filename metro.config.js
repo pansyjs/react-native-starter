c3ce030dbf6212413d03cb9daaa9635f08c44eec
wangxingkang <wang_xingkang@qq.com>
2024-09-13 12:26:20 +0800

    chore: ues pnpm

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
 /**
  * Metro configuration
@@ -6,6 +7,10 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
