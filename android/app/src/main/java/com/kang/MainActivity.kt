639c723568315788c3db1dcba71e584bb6e0d48e
wangxingkang <wang_xingkang@qq.com>
2024-09-17 13:13:57 +0800

    chore: rename to kang

--- android/app/src/main/java/com/kang/MainActivity.kt
+++ android/app/src/main/java/com/kang/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.testapp
+package com.kang
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "testapp"
+  override fun getMainComponentName(): String = "kang"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/kang/MainApplication.kt
+++ android/app/src/main/java/com/kang/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.testapp
+package com.kang
 
 import android.app.Application
 import com.facebook.react.PackageList
